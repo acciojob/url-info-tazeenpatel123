627c96ee037c142cbfb2d118652d1b51cffd1bc4
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-21 20:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL using the location object
+let currentURL = window.location.href;
+
+// Calculate the length of the URL
+let n = currentURL.length;
+
+// Display the length of the URL using alert in the specified format
+alert(`The length of the URL is: ${n}`);
